a87c62ae69a568047ed6eb33f78d9e73086041db
MFORCE\takuya_nakai <takuya_nakai@mforce.co.jp>
2024-10-08 09:00:33 +0900

    accept request body

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,18 @@
+import json
 def lambda_handler(event, context):
+    # OPTIONSメソッドを通すための設定
+    name = 'default'
+    if(event['body']) and (event['body'] is not None):
+        body = json.loads(event['body'])
+        try:
+            if(body['name']) and (body['name'] is not None):
+                name = body['name']
+        except KeyError:
+            print('No Name')
+
     return {
         'statusCode': 200,
-        'body': '\nhello world',
+        'body': json.dumps('\nhello world ' + name),
         'headers': {
             'Access-Control-Allow-Headers': '*',
             'Access-Control-Allow-Origin': '*',
