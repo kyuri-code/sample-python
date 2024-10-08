fcc4d55c3fbe3ca6d53fee2fb781f2c80dfa94ec
MFORCE\takuya_nakai <takuya_nakai@mforce.co.jp>
2024-10-08 10:21:09 +0900

    add check logic where body param exists

--- lambda_function.py
+++ lambda_function.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('\nhello world ' + name),
         'headers': {
-            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
             'Content-Type': 'application/json'
